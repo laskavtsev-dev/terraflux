f77e39a0b5193e0278501caf48d17b1818aa62fc
Alexander Laskavtsev <las@agrain.ua>
2024-05-12 14:17:12 +0000

    afasdf

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
-module "kind_cluster" {
-  source = "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
+module "tls_private_key" {
+  source    = "github.com/laskavtsev-dev/tf-hashicorp-tls-keys"
+  algorithm = "RSA"
 }
+
+module "github_repository" {
+  source                   = "github.com/laskavtsev-dev/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux"
+}
+
+module "kind_cluster" {
+  source = "github.com/den-vasyliev/tf-kind-cluster"
+}
+
+module "flux_bootstrap" {
+  source            = "github.com/laskavtsev-dev/tf-fluxcd-flux-bootstrap"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  github_token      = var.GITHUB_TOKEN
+}
+
