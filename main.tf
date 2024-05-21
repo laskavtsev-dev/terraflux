8c2ca0b8331fe75932fdc95e6ba304861edd0908
Alexander Laskavtsev <las@agrain.ua>
2024-05-21 06:48:28 +0000

    delete kind-cluster-config

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-module "tls_private_key" {
-  source    = "github.com/laskavtsev-dev/tf-hashicorp-tls-keys"
-  algorithm = "RSA"
-}
-
 module "github_repository" {
   source                   = "github.com/laskavtsev-dev/tf-github-repository"
   github_owner             = var.GITHUB_OWNER
@@ -12,14 +7,21 @@ module "github_repository" {
   public_key_openssh_title = "flux"
 }
 
+module "tls_private_key" {
+  source    = "github.com/laskavtsev-dev/tf-hashicorp-tls-keys"
+  algorithm = "RSA"
+}
+
 module "kind_cluster" {
-  source = "github.com/den-vasyliev/tf-kind-cluster"
+  source = "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
 }
 
 module "flux_bootstrap" {
   source            = "github.com/laskavtsev-dev/tf-fluxcd-flux-bootstrap"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key       = module.tls_private_key.private_key_pem
+  config_path       = "kind-cluster-config"
   github_token      = var.GITHUB_TOKEN
 }
+
 
